25d508a7f489568a37695fef4091d9747e3091a1
Anthony Rouneau <angeal1105@gmail.com>
2017-04-20 00:42:47 +0200

    Fixed unit tests

--- pytgf/game/realtime/__init__.py
+++ pytgf/game/realtime/__init__.py
@@ -1,3 +1,3 @@
 # noinspection PyUnresolvedReferences,PyPep8Naming
-from pytgf.game.api import API as RealTimeAPI
+from pytgf.game.api import API
 from pytgf.game.realtime.rt_mainloop import RealTimeMainLoop
